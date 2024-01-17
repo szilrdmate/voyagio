6bf9edc88c850fcc0c5b1f3fa1bd15fe943a8faf
Szilárd Máté <mate.szilard.mark@gmail.com>
2024-01-17 09:56:00 +0100

    form 95%

--- src/hooks/useInputChange.ts
+++ src/hooks/useInputChange.ts
@@ -1,24 +1,23 @@
 // hooks/useInputChange.ts
 import { Dispatch, SetStateAction } from "react";
-import { ItineraryAction } from "../types/ItineraryTypes";
+import { FormState, ItineraryAction } from "../types/ItineraryTypes";
 
 export const useInputChange = (
   dispatch: Dispatch<ItineraryAction>,
   setErrors: Dispatch<SetStateAction<{ [key: string]: string }>>
 ) => {
-  const handleInputChange = (field: string) => (e: React.ChangeEvent<HTMLInputElement> ) => {
+  const handleInputChange = (field: keyof FormState) => (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch({ type: "SET_FIELD", field, value: e.target.value });
     setErrors((prevErrors) => ({ ...prevErrors, [field]: "" }));
   };
-  const handleButtonInputChange = (field: string, value: string) => () => {
+  const handleButtonInputChange = (field: keyof FormState, value: string) => () => {
     dispatch({ type: "SET_FIELD", field, value });
     setErrors((prevErrors) => ({ ...prevErrors, [field]: "" }));
   };
-  const handleMultipleChoiceChange = (field: string, value: string) => {
+  const handleMultipleChoiceChange = (field: keyof FormState, value: string) => {
     dispatch({ type: "TOGGLE_ARRAY_FIELD_ITEM", field, value });
     setErrors((prevErrors) => ({ ...prevErrors, [field]: "" }));
   };
 
-
-  return {handleInputChange, handleButtonInputChange, handleMultipleChoiceChange} ;
+  return { handleInputChange, handleButtonInputChange, handleMultipleChoiceChange };
 };
